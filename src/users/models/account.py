376ca27bddb11156e8c831afa3620f26af28b8cc
Maks <maksimuspiter@gmail.com>
2023-06-22 18:30:55 +0300

    chenge + add account

--- src/users/models/account.py
+++ src/users/models/account.py
@@ -6,32 +6,36 @@ from uuid import uuid4
 from .city import City
 
 
-class AccountManager(models.Manager):
-    def create_account(
-        self,
-        city_name: str,
-        country_name: str,
-        user: dict[str, str]
-    ):
-        city = City.objects.get(
-            name=city_name, country__name=country_name
-        )  # TODO: need unique fields
-        new_user = User.objects.create_user(**user)
-        new_account = self.model(user=new_user, city=city)
-        new_account.save(using=self._db)
-        return new_account
+# class AccountManager(models.Manager):
+#     def create_account(
+#         self,
+#         city_name: str,
+#         country_name: str,
+#         user: dict[str, str]
+#     ):
+#         city = City.objects.get(
+#             name=city_name, country__name=country_name
+#         )  # TODO: need unique fields
+#         new_user = User.objects.create_user(**user)
+#         new_account = self.model(user=new_user, city=city)
+#         new_account.save(using=self._db)
+#         return new_account
 
 
 class Account(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4)
-    user: User = models.OneToOneField(
-        User, related_name="account", on_delete=models.CASCADE
-    )
-    city = models.ForeignKey(City, on_delete=models.PROTECT)
-    followers = models.ManyToManyField("self", blank=True)  # TODO: naming
-    black_list = models.ManyToManyField("self", blank=True)
+    user = models.OneToOneField(User, related_name="account", on_delete=models.CASCADE)
 
-    objects = AccountManager()
+    bio = models.TextField(blank=True, null=True, verbose_name="Биография")
+    birthday = models.DateTimeField(null=True, blank=True, verbose_name="День рождения")
+    photo = models.ImageField(
+        upload_to="uploads/account_img/", verbose_name="Фото", blank=True, null=True
+    )
+    status = models.CharField(max_length=100, verbose_name="Статус", blank=True)
+
+    city = models.ForeignKey(City, on_delete=models.PROTECT, verbose_name="Город")
+    black_list = models.ManyToManyField("self", blank=True, verbose_name="Игнор лист")
+    # objects = AccountManager()
 
     def __str__(self):
         return self.user.username
--- src/users/models/city.py
+++ src/users/models/city.py
@@ -1,21 +1,19 @@
 from django.db import models
 
-from .country import Country
-
 
 class City(models.Model):
     name = models.CharField(max_length=100)
-    country = models.ForeignKey(
-        Country, on_delete=models.CASCADE, related_name="+"
-    )
-
-    objects = models.Manager()
+    region = models.ForeignKey("Region", on_delete=models.CASCADE, related_name="city")
 
     def get_full_name(self):
-        return f"{self.name}, {self.country.name}"
+        return f"{self.name}, {self.region.name}"
 
     def __str__(self):
         return self.name
 
     class Meta:
-        unique_together = ("name", "country")
+        unique_together = ("name", "region")
+
+
+class Region(models.Model):
+    name = models.CharField(max_length=100)
