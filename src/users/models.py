098823da1ceb27b74bb5d8827cc5b64d2e232416
Maks <maksimuspiter@gmail.com>
2023-06-22 20:14:41 +0300

    add SocialNetworkLinks

--- src/users/models.py
+++ src/users/models.py
@@ -10,15 +10,9 @@ class AccountManager(models.Manager):
     """
     Custom account manager.
     """
-    def create_account(
-        self,
-        city_name: str,
-        country_name: str,
-        user: dict[str, str]
-    ):
-        city = City.objects.get(
-            name=city_name, country__name=country_name
-        )
+
+    def create_account(self, city_name: str, country_name: str, user: dict[str, str]):
+        city = City.objects.get(name=city_name, country__name=country_name)
         new_user = User.objects.create_user(**user)
         new_account = self.model(user=new_user, city=city)
         new_account.save(using=self._db)
@@ -29,8 +23,11 @@ class Account(models.Model):
     """
     Account model.
     """
+
     id = models.UUIDField(primary_key=True, default=uuid4)
-    user: User = models.OneToOneField(User, related_name="account", on_delete=models.CASCADE)
+    user: User = models.OneToOneField(
+        User, related_name="account", on_delete=models.CASCADE
+    )
 
     bio = models.TextField(blank=True, null=True, verbose_name="Биография")
     birthday = models.DateTimeField(null=True, blank=True, verbose_name="День рождения")
@@ -45,3 +42,33 @@ class Account(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+SOCIAL_NETWORK_LINK_NAME = (
+    ("VK", "Вконтакте"),
+    ("INST", "Instagram"),
+    ("FB", "Facebook"),
+    ("WA", "WhatsApp"),
+    ("YT", "YouTube"),
+)
+
+
+class SocialNetworkLinks(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4)
+    account = models.ForeignKey(
+        Account,
+        verbose_name="Пользователь",
+        related_name="social_network_links",
+        on_delete=models.CASCADE,
+    )
+    name = models.CharField(
+        max_length=4, choices=SOCIAL_NETWORK_LINK_NAME, default="VK"
+    )
+    links = models.URLField()
+
+    def __str__(self):
+        return f"{self.get_name_display()}:{self.links}"
+
+    class Meta:
+        verbose_name = "Ссылка на социальные сети"
+        verbose_name_plural = "Ссылки на социальные сети"
