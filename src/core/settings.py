187961cd08da1acd6da98a8d2b2692e611fe0a57
Michael Nightingale <suslanchikmopl@gmail.com>
2023-06-23 23:07:18 +0500

    add session auth for tests

--- src/core/settings.py
+++ src/core/settings.py
@@ -146,7 +146,8 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
         # "rest_framework.authentication.TokenAuthentication",
-        "rest_framework_simplejwt.authentication.JWTAuthentication"
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
     ],
     "DEFAULT_PERMISSIONS_CLASSES": [
         "rest_framework.permissions.AllowAny",
