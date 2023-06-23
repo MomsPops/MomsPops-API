e7c16a74b28669e12733f59a8e4c3908dbf652cc
Michael Nightingale <suslanchikmopl@gmail.com>
2023-06-23 23:07:33 +0500

    add views

--- src/locations/serializer.py
+++ src/locations/serializer.py
@@ -1,12 +1,26 @@
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
-from .models import City   # , Region
+from .models import City, Region
 
 
-class CitiesListSerializer(ModelSerializer):
+class CityListSerializer(ModelSerializer):
     region_name = serializers.CharField(source="region.name")
 
     class Meta:
         model = City
         fields = ("name", "region_name")
+
+
+class CityDetailSerializer(ModelSerializer):
+    class Meta:
+        model = City
+        fields = ("id", "name")
+
+
+class LocationListSerializer(ModelSerializer):
+    cities = CityDetailSerializer(many=True)
+
+    class Meta:
+        model = Region
+        fields = ("id", "name", "cities")
