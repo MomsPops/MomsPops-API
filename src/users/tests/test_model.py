7045accde652fd2133e1378cbb60b4d974a794e5
Maksim_Tataurov <maksimuspiter@gmail.com>
2023-06-23 17:13:01 +0300

    add test

--- src/users/tests/test_model.py
+++ src/users/tests/test_model.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.models import User
-from django.db import transaction
 from django.test import TestCase
 from django.contrib.auth import authenticate
 from users.models import Account
@@ -16,11 +15,14 @@ class AccountTest(TestCase):
 
     def create_account_test(self):
         user1 = User.objects.create(username="test_user1")
-        user1 = User.objects.create(username="test_user1")
-        user2 = User.objects.last()
+        # user2 = User.objects.create(username="test_user2")
         account1 = Account.objects.create(user=user1)
 
         self.assertTrue(account1 is not None)
         self.assertEqual(Account.objects.count(), 1)
 
-        # account2 = Account.objects.create_account
+        user_data = {"username": "created_test_user1", "email": "test@mail.com"}
+
+        account2 = Account.objects.create_account(user_data=user_data)
+        self.assertTrue(account2 is not None)
+        self.assertEqual(Account.objects.count(), 2)
