149009d13bee56cefb47b90ac09e6fb59f741d25
Michael Nightingale <suslanchikmopl@gmail.com>
2023-06-24 11:37:53 +0500

    change urls

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,4 +1,5 @@
 from rest_framework.routers import DefaultRouter
+from django.urls import path
 
 from .views import AccountViewSet
 
@@ -7,7 +8,8 @@ router.register("accounts", AccountViewSet)
 
 
 urlpatterns = [
+    path("accounts/", AccountViewSet.as_view({"post": "create"}), name='accounts_create'),
+    path("accounts/update/", AccountViewSet.as_view({"patch": "update"}), name="accounts_update"),
+    path("accounts/me/", AccountViewSet.as_view({"get": "retrieve"}), name="accounts_me"),
 
 ]
-
-urlpatterns += router.urls
