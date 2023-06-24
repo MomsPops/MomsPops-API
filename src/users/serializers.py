3b5c80e97ba9bb9756f15e78131323dd2d98045f
Michael Nightingale <suslanchikmopl@gmail.com>
2023-06-24 11:38:19 +0500

    add serializers

--- src/users/serializers.py
+++ src/users/serializers.py
@@ -6,23 +6,35 @@ from .models import Account
 
 
 class UserCreateSerializer(ModelSerializer):
+    first_name = serializers.CharField(max_length=30, allow_null=False)
+    last_name = serializers.CharField(max_length=30, allow_null=False)
 
     class Meta:
         model = User
         fields = ("username", "password", "email", "first_name", "last_name")
 
 
+class UserDetailSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("username", "email", "first_name", "last_name")
+
+
 class AccountCreateSerializer(ModelSerializer):
-    country_name = serializers.CharField(max_length=100, source="city.country.name")
+    region_name = serializers.CharField(max_length=100, source="city.region.name")
     city_name = serializers.CharField(max_length=100, source="city.name")
     user = UserCreateSerializer()
 
     class Meta:
         model = Account
-        fields = ("user", "city_name", "country_name")
+        fields = ("user", "city_name", "region_name")
 
 
 class AccountDetailSerializer(ModelSerializer):
+    user = UserDetailSerializer()
+    city_name = serializers.CharField(source="city.name")
+    region_name = serializers.CharField(source="city.region.name")
+
     class Meta:
         model = Account
-        fields = "__all__"
+        fields = ("user", "city_name", "region_name")
