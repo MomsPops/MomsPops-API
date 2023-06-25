da6e8e1f232edfb07de786d073134c99e15e64b3
Григорий Шабалкин <ShadowMoses1314@mail.ru>
2023-06-25 20:39:43 +0600

    Beta functions Внес Изменения необходимые в функции, написал тесты, гляньте пожалуйста

--- src/coordinates/service/city_by_google.py
+++ src/coordinates/service/city_by_google.py
@@ -6,18 +6,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-def get_location_details(geo_point):
+def get_location_details(latitude: float, longitude: float) -> str:
     api_key = os.getenv("GOOGLE_MAPS_API_KEY")
     gmaps = googlemaps.Client(api_key)
+    geo_point = f"{latitude}, {longitude}"
     reverse_geocode_result = gmaps.reverse_geocode(geo_point, language='ru')
-
-    address_components = reverse_geocode_result[0]['address_components']
 
     city = ''
     street = ''
     district = ''
 
-    for component in address_components:
+    for component in reverse_geocode_result[0]['address_components']:
         types = component['types']
         if 'locality' in types:
             city = component['long_name']
@@ -26,10 +25,14 @@ def get_location_details(geo_point):
         if 'sublocality' in types:
             district = component['long_name']
 
-    result = "Город: " + city
+    result_parts = []
+    if city:
+        result_parts.append(f"Город: {city}")
     if street:
-        result += ",\nУлица: " + street
+        result_parts.append(f"Улица: {street}")
     if district:
-        result += ",\nРайон: " + district
+        result_parts.append(f"Район: {district}")
+
+    result = ",\n".join(result_parts)
 
     return result
