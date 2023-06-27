c2edb6af7e0f21c6577c86e981780193105aa451
Anton <A60874022@yandex.ru>
2023-06-27 20:59:50 +0500

    reactions

--- src/reactions/tests/test_model.py
+++ src/reactions/tests/test_model.py
@@ -23,16 +23,16 @@ class ReactionItemModelTest(TestCase):
         """Проверка что картинка сохраняеться в базу данных"""
         count = ReactionItem.objects.count()
         small_gif = (
-             b'\x47\x49\x46\x38\x39\x61\x02\x00'
-             b'\x01\x00\x80\x00\x00\x00\x00\x00'
-             b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
-             b'\x00\x00\x00\x2C\x00\x00\x00\x00'
-             b'\x02\x00\x01\x00\x00\x02\x02\x0C'
-             b'\x0A\x00\x3B'
+            b'\x47\x49\x46\x38\x39\x61\x02\x00'
+            b'\x01\x00\x80\x00\x00\x00\x00\x00'
+            b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
+            b'\x00\x00\x00\x2C\x00\x00\x00\x00'
+            b'\x02\x00\x01\x00\x00\x02\x02\x0C'
+            b'\x0A\x00\x3B'
         )
         uploaded = SimpleUploadedFile(
             name='small.gif',
             content=small_gif,
             content_type='image/gif)')
         ReactionItem.objects.create(image=uploaded)
-        self.assertEqual(ReactionItem.objects.count(), count+1)
+        self.assertEqual(ReactionItem.objects.count(), count + 1)
