52859c717988984fb02988ca75a27688c32efde8
Michael Nightingale <suslanchikmopl@gmail.com>
2023-07-08 22:48:53 +0500

    code refactoring

--- src/notifications/urls.py
+++ src/notifications/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", PersonalNotificationViewSet.as_view({'get': 'list'}), name='get_notifications'),
     path("<int:pk>/", PersonalNotificationViewSet.as_view({'get': 'retrieve'}), name='get_notification'),
     path("<int:pk>/viewed/", PersonalNotificationViewSet.as_view({'post': 'viewed'}), name='change_notification'),
+
 ]
