803580303a3990ce130383ad1e1f9372d44ca578
Michael Nightingale <suslanchikmopl@gmail.com>
2023-07-08 22:49:06 +0500

    add black list urls

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,14 +1,18 @@
 from django.urls import path, include
 
-from .views import AccountViewSet
-from .routers import AccountRouter
+from .views import AccountViewSet, BlackListViewSet
+from .routers import AccountRouter, BlackListRouter
 
 
-router = AccountRouter()
-router.register("accounts", AccountViewSet, "accounts")
+account_router = AccountRouter()
+account_router.register("accounts", AccountViewSet, "accounts")
+
+black_list_router = BlackListRouter()
+black_list_router.register("black-list", BlackListViewSet, "black_list")
 
 
 urlpatterns = [
-    path("", include(router.urls)),
+    path("", include(account_router.urls)),
+    path("", include(black_list_router.urls)),
 
 ]
