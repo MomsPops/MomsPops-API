8c72efbdc6534802c1ebd33e05c11a2cd6bbd787
Michael Nightingale <suslanchikmopl@gmail.com>
2023-07-08 22:49:21 +0500

    add user black list tests

--- src/users/tests/test_model.py
+++ src/users/tests/test_model.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from rest_framework.test import APITestCase
 from django.core.exceptions import ObjectDoesNotExist
 from service.fixtues import TestAccountFixture
@@ -62,3 +63,38 @@ class TestAccountModel(TestAccountFixture, APITestCase):
         self.assertTrue(self.superuser_account.user.is_active)
         Account.objects.activate(self.superuser_account)
         self.assertTrue(self.superuser_account.user.is_active)
+
+
+class TestBlockUserModel(TestAccountFixture, APITestCase):
+
+    def test_block_user(self):
+        amount_blocked_before = self.user_account.black_list.count()
+        Account.objects.block_user(self.user_account, self.user2.username)
+        self.user_account.refresh_from_db()
+
+        self.assertEqual(self.user_account.black_list.count() - amount_blocked_before, 1)
+        self.assertTrue(self.user_account.black_list.filter(user=self.user2).exists())
+
+    def test_block_user_fail_yourself(self):
+        with self.assertRaises(ValueError):
+            Account.objects.block_user(account=self.user_account, username=self.user_account.user.username)
+
+    def test_block_user_fail_not_found(self):
+        with self.assertRaises(Http404):
+            Account.objects.block_user(account=self.user_account, username="Not_found_name")
+
+    def test_user_unblock(self):
+        amount_blocked_before = self.user_account.black_list.count()
+        Account.objects.block_user(self.user_account, self.user2.username)
+        self.user_account.refresh_from_db()
+        self.assertEqual(self.user_account.black_list.count() - amount_blocked_before, 1)
+        Account.objects.unblock_user(self.user_account, self.user2.username)
+        self.assertEqual(self.user_account.black_list.count(), amount_blocked_before)
+
+    def test_unblock_user_fail_yourself(self):
+        with self.assertRaises(ValueError):
+            Account.objects.unblock_user(account=self.user_account, username=self.user_account.user.username)
+
+    def test_unblock_user_fail_not_found(self):
+        with self.assertRaises(Http404):
+            Account.objects.unblock_user(account=self.user_account, username="Not_found_name")
--- src/users/tests/test_view.py
+++ src/users/tests/test_view.py
@@ -1,5 +1,6 @@
 from rest_framework.test import APITestCase
 from django.urls import reverse
+from http import HTTPStatus
 
 from users.models import Account
 from service.fixtues import TestAccountFixture
@@ -20,7 +21,7 @@ class TestAccountView(TestAccountFixture, APITestCase):
             }
         }
         response = self.client.post(path=reverse("accounts"), data=data, format='json')
-        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.status_code, HTTPStatus.CREATED)
         user_data = data['user']
         user_data.pop("password")
         data['user'] = user_data
@@ -36,13 +37,13 @@ class TestAccountView(TestAccountFixture, APITestCase):
             }
         }
         response = self.client.post(path=reverse("accounts"), data=data, format='json')
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.status_code, HTTPStatus.BAD_REQUEST)
         self.assertEqual(response.json(), {'user': {'first_name': ['This field is required.'],
                                            'last_name': ['This field is required.']}})
 
     def test_account_retrieve(self):
         response = self.user_client.get(reverse("accounts_me"))
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertEqual(response.json(), {
             "city_name": self.city1.name,
             "region_name": self.city1.region.name,
@@ -56,7 +57,7 @@ class TestAccountView(TestAccountFixture, APITestCase):
 
     def test_account_deactivate(self):
         response = self.user_client.delete(reverse("accounts_me"))
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertEqual(response.json(), {"detail": "User deactivated."})
         self.user.refresh_from_db()
         self.assertEqual(self.user.is_active, False)
@@ -65,7 +66,7 @@ class TestAccountView(TestAccountFixture, APITestCase):
         pre_accounts_count = Account.objects.count()
         response = self.user_client.delete(reverse("accounts_me"), {"delete": True})
         self.assertEqual(response.json(), {"detail": "User deleted."})
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertEqual(pre_accounts_count - Account.objects.count(), 1)
 
     def test_account_update(self):
@@ -76,7 +77,89 @@ class TestAccountView(TestAccountFixture, APITestCase):
             }
         }
         response = self.user_client.patch(reverse("accounts_me"), data=partial_data, format='json')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
         self.user.refresh_from_db()
         self.assertEqual(self.user.last_name, partial_data['user']['last_name'])
         self.assertEqual(self.user.first_name, partial_data['user']['first_name'])
+
+
+class TestBlockUserViews(TestAccountFixture, APITestCase):
+
+    def test_block_user(self):
+        response = self.user_client.post(
+            path=reverse("black_list"),
+            data={"username": self.user3.username}
+        )
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.json(), {"detail": "User blocked successfully."})
+        self.user_account.refresh_from_db()
+        self.assertTrue(self.user_account.black_list.filter(user=self.user3).exists())
+
+    def test_block_user_fail_yourself(self):
+        response = self.user_client.post(
+            path=reverse("black_list"),
+            data={"username": self.user.username}
+        )
+        self.assertEqual(response.status_code, HTTPStatus.BAD_REQUEST)
+        self.assertEqual(response.json(), {"detail": "You cannot add yourself to a black list"})
+
+    def test_block_user_fail_not_found(self):
+        response = self.user_client.post(
+            path=reverse("black_list"),
+            data={"username": "a[sdkasdolas"}
+        )
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+
+    def test_unblock_user(self):
+        response = self.user_client.post(
+            path=reverse("black_list"),
+            data={"username": self.user3.username}
+        )
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.json(), {"detail": "User blocked successfully."})
+        self.user_account.refresh_from_db()
+        self.assertTrue(self.user_account.black_list.filter(user=self.user3).exists())
+
+        response_delete = self.user_client.delete(
+            path=reverse("black_list"),
+            data={"username": self.user3.username}
+        )
+        self.assertEqual(response_delete.status_code, HTTPStatus.OK)
+        self.assertEqual(response_delete.json(), {"detail": "User unblocked successfully."})
+        self.user_account.refresh_from_db()
+        self.assertFalse(self.user_account.black_list.filter(user=self.user3).exists())
+
+    def test_unblock_user_fail_yourself(self):
+        response = self.user_client.delete(
+            path=reverse("black_list"),
+            data={"username": self.user.username}
+        )
+        self.assertEqual(response.status_code, HTTPStatus.BAD_REQUEST)
+        self.assertEqual(response.json(), {"detail": "You cannot remove yourself from a black list"})
+
+    def test_unblock_user_fail_not_found(self):
+        response = self.user_client.delete(
+            path=reverse("black_list"),
+            data={"username": "a[sdkasdolas"}
+        )
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+
+    def test_blask_list_list(self):
+        response = self.user_client.post(
+            path=reverse("black_list"),
+            data={"username": self.user3.username}
+        )
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.json(), {"detail": "User blocked successfully."})
+        self.user_account.refresh_from_db()
+        self.assertTrue(self.user_account.black_list.filter(user=self.user3).exists())
+        response_list = self.user_client.get(path=reverse("black_list"))
+        self.assertEqual(response_list.status_code, HTTPStatus.OK)
+        self.assertEqual(
+            len(response_list.json()),
+            self.user_account.black_list.count()
+        )
+        self.assertIn(
+            self.user3.username,
+            [i['user']['username'] for i in response_list.json()]
+        )
