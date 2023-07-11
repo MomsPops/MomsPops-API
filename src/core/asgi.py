cb179050960244832f9416e28ca0c16055fb862c
Павел Михайлович Пеньтюк <Pentiuk.Pavel@ORG.local>
2023-07-11 13:48:13 +0300

    channels configuration

--- src/core/asgi.py
+++ src/core/asgi.py
@@ -1,7 +1,19 @@
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from channels.security.websocket import AllowedHostsOriginValidator
+
+from chats import routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
+        )
+    }
+)
