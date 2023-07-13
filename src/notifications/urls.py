f7aba1afd2c2f647d5315f6f723145d844ac294d
Michael Nightingale <suslanchikmopl@gmail.com>
2023-07-13 12:17:10 +0500

    remove unnecessary code

--- src/notifications/urls.py
+++ src/notifications/urls.py
@@ -1,11 +1,3 @@
-from django.urls import path
-
-from .views import PersonalNotificationViewSet
-
-
 urlpatterns = [
-    path("", PersonalNotificationViewSet.as_view({'get': 'list'}), name='get_notifications'),
-    path("<int:pk>/", PersonalNotificationViewSet.as_view({'get': 'retrieve'}), name='get_notification'),
-    path("<int:pk>/viewed/", PersonalNotificationViewSet.as_view({'post': 'viewed'}), name='change_notification'),
 
 ]
