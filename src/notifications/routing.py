3dab4297a8b147b7b30f5d09b6795f541378328a
michael7nightingale <suslanchikmopl@gmail.com>
2023-07-14 10:47:52 +0500

    update routing

--- src/notifications/routing.py
+++ src/notifications/routing.py
@@ -4,6 +4,6 @@ from .consumers import NotificationConsumer
 
 
 websocket_urlpatterns = [
-    re_path(r"ws/notifications/", NotificationConsumer.as_asgi(), name='notification_consumer'),
+    re_path(r"ws/notifications", NotificationConsumer.as_asgi(), name='notification_consumer'),
 
 ]
