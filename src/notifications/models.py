33a39097089f60a7d3d64f9b6b88a7d5a8280917
michael7nightingale <suslanchikmopl@gmail.com>
2023-07-14 19:03:44 +0500

    refactor models

--- src/notifications/models.py
+++ src/notifications/models.py
@@ -14,15 +14,14 @@ class Notification(UUIDModel, TimeCreateModel):
         verbose_name="Аккаунт",
         to="users.Account",
         related_name="notifications",
-        through='NotificationAccount'
+        through="NotificationAccount",
     )
-    sender = notification = models.ForeignKey(
-        verbose_name="Отправитель",
+    sender = models.ForeignKey(
+        verbose_name="sender",
         to="users.Account",
         on_delete=models.CASCADE,
         null=True
     )
-    viewed = models.BooleanField("Прочитано", default=False)
 
     objects = models.Manager()
 
@@ -34,46 +33,36 @@ class Notification(UUIDModel, TimeCreateModel):
         verbose_name_plural = "Уведомления"
 
 
-class NotificationAccountManager(models.Manager):
-    def get(self, *args, **kwargs):
-        return (
-            super()
-            .select_related("notification", "account", "account__user")
-            .get(*args, **kwargs)
-        )
+class NotificationAccount(models.Model):
+    """
+    Model for notification and account relation.
+    """
 
+    account = models.ForeignKey(
+        verbose_name="Аккаунт",
+        to="users.Account",
+        on_delete=models.CASCADE
+    )
 
-# class NotificationAccount(models.Model):
-#     """
-#     Model for notification and account relation.
-#     """
-#
-#     account = models.ForeignKey(
-#         verbose_name="Аккаунт",
-#         to="users.Account",
-#         on_delete=models.CASCADE
-#     )
-#
-#     notification = models.ForeignKey(
-#         verbose_name="Уведомление",
-#         to=Notification,
-#         on_delete=models.CASCADE
-#     )
-#
-#     viewed = models.BooleanField("Прочитано", default=False)
-#
-#     objects = models.Manager()
-#     notification_account_manager = NotificationAccountManager()
-#
-#     def is_viewed(self):
-#         """Sets viewed to `True`."""
-#         self.viewed = True
-#         self.save()
-#
-#     def __str__(self):
-#         return f'{self.account}: {self.notification}'
-#
-#     class Meta:
-#         unique_together = ('account', 'notification')
-#         verbose_name = 'Уведомление для пользователя'
-#         verbose_name_plural = 'Уведомления для пользователей'
+    notification = models.ForeignKey(
+        verbose_name="Уведомление",
+        to=Notification,
+        on_delete=models.CASCADE
+    )
+    viewed = models.BooleanField("Viewed", default=False, null=True)
+
+    objects = models.Manager()
+    # notification_account_manager = NotificationAccountManager()
+
+    def is_viewed(self):
+        """Sets viewed to `True`."""
+        self.viewed = True
+        self.save()
+
+    def __str__(self):
+        return f'{self.account}: {self.notification}'
+
+    class Meta:
+        unique_together = ('account', 'notification')
+        verbose_name = 'Уведомление для пользователя'
+        verbose_name_plural = 'Уведомления для пользователей'
