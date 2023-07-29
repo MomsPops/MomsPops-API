a05138fb95a2fb64cd62466a7fc35c27026380ab
michael7nightingale <suslanchikmopl@gmail.com>
2023-07-30 00:09:33 +0500

    move loading dotenv to manage.py for better control

--- src/manage.py
+++ src/manage.py
@@ -2,9 +2,11 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from dotenv import load_dotenv
 
 
 def main():
+    load_dotenv(".dev.env")
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
     try:
