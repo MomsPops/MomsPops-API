0f333dd48a616368c046ffd7d256ff13b02219b5
michael7nightingale <suslanchikmopl@gmail.com>
2023-08-05 14:09:20 +0500

    refactor notification consumers

--- src/notifications/consumers.py
+++ src/notifications/consumers.py
@@ -3,9 +3,13 @@ from channels.db import database_sync_to_async
 from asgiref.sync import sync_to_async
 import json
 
+from django.db.transaction import atomic
+from djangochannelsrestframework.decorators import action
+
 from users.models import Account
-from .models import Notification
-from .serializers import NotificationDetailSerializer
+from service.consumers import BaseConsumer
+from .models import Notification, NotificationAccount
+from .serializers import NotificationDetailSerializer, NotificationCreateSerializer, NotificationAccountListSerializer
 
 
 @database_sync_to_async
@@ -14,35 +18,8 @@ def get_account_by_id(account_id: str):
 
 
 class NotificationConsumer(AsyncJsonWebsocketConsumer):
-    def __init__(self, *args, **kwargs):
-        """Redefine model to define custom instance attributes."""
-        super().__init__(*args, **kwargs)
-        self.user = None
-        self.account = None
-        self.account_id = None
 
-    async def connect(self) -> None:
-        """On socket connection. Allowed only for authorized users."""
-        await self.accept()
-        if self.scope['user'] is None:
-            await self.close()
-            return
-        self.user = self.scope['user']
-        self.account = self.user.account    # type: ignore
-        self.account_id = str(self.account.id)
-        await self.channel_layer.group_add(
-            self.account_id,
-            self.channel_name
-        )
-
-    async def disconnect(self, code) -> None:
-        """On disconnect socket."""
-        await self.channel_layer.group_discard(
-            self.account_id,
-            self.channel_name
-        )
-        await self.close(code)
-
+    @atomic
     async def receive_json(self, content, *args, **kwargs) -> None:
         """
         Send notification to all account. First N.instance and its serializer are created.
@@ -80,3 +57,69 @@ class NotificationConsumer(AsyncJsonWebsocketConsumer):
         """Receive notification by a single account websocket."""
         message = event['message']
         await self.send(text_data=json.dumps(message))
+
+
+class NotificationConsumer(BaseConsumer):
+    notification_actions = {
+        "notification_list": NotificationAccountListSerializer,
+        "notification_create": NotificationCreateSerializer,
+        "notification_detail": NotificationDetailSerializer,
+
+    }
+
+    async def send_notification(self, event) -> None:
+        """Receive notification by a single account websocket."""
+        message = event['message']
+        await self.send(text_data=json.dumps(message))
+
+    @database_sync_to_async
+    def get_notification_serializer(self, action_kwargs, *args, **kwargs):
+        return
+
+    @database_sync_to_async
+    def get_all_notifications_by_account(self, account):
+        return [
+            na for na in NotificationAccount.notification_account_manager.get_all_by_account(account)
+        ]
+
+    @action
+    async def notification_list(self, **kwargs):
+        action_kwargs = self.prepare_data(**kwargs)
+        serializer = await self.get_reaction_serializer(
+            action_kwarg=action_kwargs,
+            instance=await self.get_all_notifications_by_account(self.account),
+            many=True
+        )
+        serializer.is_valid(raise_exception=True)
+        data = await self.get_serializer_data(serializer)
+        data.update(action=action_kwargs['action'])
+        await self.send_json(content=data)
+
+    @action
+    @atomic
+    async def notification_create(self, **kwargs):
+        action_kwargs = self.prepare_data(**kwargs)
+        create_serializer = await self.get_reaction_serializer(
+            action_kwarg=action_kwargs,
+            data=kwargs
+        )
+        create_serializer.is_valid(raise_exception=True)
+        notification = await self.save_serializer(create_serializer)
+        detail_serializer = await self.get_serializer_data(
+            action_kwargs={
+                "action": "notification_detail",
+                "request_id": action_kwargs['request_id']
+            },
+            instance=notification
+        )
+        detail_serializer_data = await self.get_serializer_data(detail_serializer)
+        for account in create_serializer.validated_data.get('accounts'):
+            account_id = account.validated_data.get("account_id")
+            account = await sync_to_async(Account.objects.get)(id=account_id)
+            await self.channel_layer.group_send(
+                str(account.id),
+                {
+                    "type": "update_messages",
+                    "notification": detail_serializer_data
+                }
+            )
--- src/notifications/serializers.py
+++ src/notifications/serializers.py
@@ -1,9 +1,22 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
+from users.models import Account
+from users.serializers import AccountIdSerializer
 from .models import Notification, NotificationAccount
 
 
 class NotificationDetailSerializer(ModelSerializer):
+    """
+    Serializer for notifications.
+    """
+
+    class Meta:
+        model = Notification
+        fields = ("id", 'text', 'link', 'time_created', "sender")
+
+
+class NotificationListSerializer(ModelSerializer):
     """
     Serializer for notifications.
     """
@@ -19,3 +32,32 @@ class NotificationAccountDetailSerializer(ModelSerializer):
     class Meta:
         model = NotificationAccount
         fields = ("notification", "viewed", "get_view_url")
+
+
+class NotificationAccountListSerializer(ModelSerializer):
+    notification = NotificationListSerializer(read_only=True)
+
+    class Meta:
+        modul = NotificationAccount
+        fields = ("notification", )
+
+
+class NotificationCreateSerializer(ModelSerializer):
+    sender_id = serializers.CharField(required=False)
+    accounts = AccountIdSerializer(many=True)
+
+    class Meta:
+        model = Notification
+        fields = ("text", "link", "sender_id", "accounts")
+
+    def save(self, **kwargs):
+        kwargs.update(self.validated_data)
+        sender = None
+        if kwargs['sender_id'] is not None:
+            sender = Account.objects.get(id=kwargs['sender_id'])
+        kwargs.pop('sender_id')
+        notification = NotificationAccount.objects.create(
+            sender=sender,
+            **kwargs
+        )
+        return notification
